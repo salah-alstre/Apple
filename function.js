a5cf288b11d21b56326dc9c0c4a4aaa992640d79
salah <salah1999fbi0@gmail.com>
2024-10-19 21:37:38 +0300

    Initial commit

---
+++ function.js
@@ -0,0 +1,7 @@
+function imgSlider (anything){
+    document.querySelector('.iphone').src = anything;
+}
+function changeCircle (color){
+    const circle = document.querySelector(".circle");
+    circle.style.background = color;
+}
